8ad7b0603e4ab5b0085447f598e5a6b1a8c3af2c
splautH <84276285+splautH@users.noreply.github.com>
2021-05-16 23:43:53 +0900

    arisubot

--- discordbot.py
+++ discordbot.py
@@ -16,6 +16,10 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
+    
+    @bot.command()
+async def ねこ(ctx):
+    await ctx.send('にゃあ')
 
 
 bot.run(token)
